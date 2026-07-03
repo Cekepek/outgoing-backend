1013695f241b659ac135a49a25962f46bfcbfede
Cekepek <ckpk7171@gmail.com>
2026-07-03 15:49:46 +0700

    error on error handling get_exchange_rate

--- app/routers/catalogue.py
+++ app/routers/catalogue.py
@@ -3,7 +3,7 @@ from http.client import HTTPException
 from fastapi import APIRouter
 import httpx
 
-from app.schemas import BankItem, BankRequest, CatalogueItem, CatalogueRequest, ResponseSchema
+from app.schemas import BankItem, BankRequest, BaseResponse, CatalogueItem, CatalogueRequest, ExchangeRateItem, RateItem, RateRequest, ResponseSchema
 from app.config import settings
 from app.utils.signature import build_request
 
@@ -74,8 +74,77 @@ async def get_bank(bank_request: BankRequest):
         return {
             "status": status,
             "message": message,
-            "data": response.json().get("locationDetail", [])
+            "data": response.json().get("locationDetail") or []
         }
     
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    
+@router.post("/get_exchange_rate", response_model=ResponseSchema[list[ExchangeRateItem]])
+async def get_exchange_rate():
+    try:
+        url = f"{settings.payment_protocol}{settings.payment_host}{settings.payment_uri}/GetEXRateList"
+        signature, body =  build_request("POST", url, {
+            "agentSessionId": ""
+        })
+        async with httpx.AsyncClient() as client:
+            response = await client.post(
+                url,
+                json=body,
+                headers={"Authorization": signature}
+            )
+            
+            status = ""
+            message = ""
+            # if(response.json().get("code") == "0"):
+            #     status = "success"
+            #     message = "Data fetched successfully"
+            # elif(response.json().get("code") != "0"):
+            #     status = "error"
+            #     message = f"code {response.json().get('code')} from third party with message: {response.json().get('message', '')}"
+        
+        return {
+            "status": status,
+            "message": message,
+            "data": response.json() or [] 
+        }
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+@router.post("/get_rate", response_model=BaseResponse[RateItem])
+async def get_rate(rate_request: RateRequest):
+    try:
+        url = f"{settings.payment_protocol}{settings.payment_host}{settings.payment_uri}/GetEXRate"
+        signature, body =  build_request("POST", url, {
+            "agentSessionId": "",
+            "transferAmount": rate_request.transfer_amount,
+            "calcBy": rate_request.calc_by,
+            "payoutCurrency": rate_request.payout_currency,
+            "paymentMode": rate_request.payment_mode,
+            "locationId": rate_request.location_id,
+            "payoutCountry": rate_request.payout_country
+        })
+        async with httpx.AsyncClient() as client:
+            response = await client.post(
+                url,
+                json=body,
+                headers={"Authorization": signature}
+            )
+            
+            status = ""
+            message = ""
+            if(response.json().get("code") == "0"):
+                status = "success"
+                message = "Data fetched successfully"
+            elif(response.json().get("code") != "0"):
+                status = "error"
+                message = f"code {response.json().get('code')} from third party with message: {response.json().get('message', '')}"
+        
+        return {
+            "status": status,
+            "message": message,
+            "data": response.json() or []
+        }
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
--- app/schemas.py
+++ app/schemas.py
@@ -1,5 +1,5 @@
-from typing import Generic, Optional, TypeVar
-from pydantic import BaseModel
+from typing import Generic, Literal, Optional, TypeVar
+from pydantic import BaseModel, Field
 from pydantic.generics import GenericModel
 
 
@@ -9,6 +9,11 @@ class ResponseSchema(GenericModel, Generic[T]):
     status: str
     message: Optional[str] = None
     data: Optional[T] = None
+    
+class BaseResponse(BaseModel, Generic[T]):
+    status: str
+    message: str
+    data: T
 
 class ConnectionBase(BaseModel):
     code: str
@@ -38,5 +43,55 @@ class BankItem(BaseModel):
     locationId: Optional[str] = None
     locationName: Optional[str] = None
     optionalField: Optional[str] = None
+    
+class RateRequest(BaseModel):
+    transfer_amount: str = Field(..., alias="transferAmount")
+    calc_by: Literal["C", "P"] = Field(..., alias="calcBy") 
+    payout_currency: str = Field(..., alias="payoutCurrency")
+    payment_mode: str = Field(..., alias="paymentMode")
+    location_id: str = Field(..., alias="locationId")
+    payout_country: str = Field(..., alias="payoutCountry")
+    class Config:
+        populate_by_name = True
+        json_schema_extra = {
+            "example": {
+                "agentSessionId": "{{agentSessionId}}",
+                "transferAmount": "100000",
+                "calcBy": "C",
+                "payoutCurrency": "SGD",
+                "paymentMode": "B",
+                "locationId": "SGPALL",
+                "payoutCountry": "SGP",
+            }
+        }
 
+class RateItem(BaseModel):
+    collect_amount: str = Field(..., alias="collectAmount")
+    collect_currency: str = Field(..., alias="collectCurrency")
+    service_charge: str = Field(..., alias="serviceCharge")
+    vat_charge: str = Field(..., alias="vatCharge")
+    transfer_amount: str = Field(..., alias="transferAmount")
+    exchange_rate: str = Field(..., alias="exchangeRate")
+    payout_amount: str = Field(..., alias="payoutAmount")
+    payout_currency: str = Field(..., alias="payoutCurrency")
+    fee_discount: str = Field(..., alias="feeDiscount")
+    additional_premium_rate: str = Field(..., alias="additionalPremiumRate")
+    settlement_rate: str = Field(..., alias="settlementRate")
+    sla_message: str
 
+    class Config:
+        populate_by_name = True
+
+class ExchangeRateItem(BaseModel):
+    send_country: str = Field(..., alias="sendCountry")
+    send_currency: str = Field(..., alias="sendCurrency")
+    send_iso2: str = Field(..., alias="sendISO2")
+    receive_country: str = Field(..., alias="receiveCountry")
+    receiver_currency: str = Field(..., alias="receiverCurrency")
+    receive_country_name: str = Field(..., alias="receiveCountryName")
+    receive_iso2: str = Field(..., alias="receiveISO2")
+    exchange_rate: str = Field(..., alias="exchangeRate")
+    last_modified: str = Field(..., alias="lastModified")
+
+    class Config:
+        populate_by_name = True
